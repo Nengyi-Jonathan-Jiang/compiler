56c29920b135b16557250225883aa54085de8fb0
Jonathan Jiang <theNerdyMango@gmail.com>
2023-03-25 13:21:46 -0500

    .

--- assembler.h
+++ assembler.h
@@ -1,56 +1,48 @@
 #pragma once
 
 #include <bitset>
+#include <string>
+#include <sstream>
+#include <map>
 #include "vm.h"
 
 class Assembler {
 public:
-    enum opcodes {
-        // MOV D    -> 0b1 010 0 00100
-        // MOV A    -> 0b1 010 0 00100
-        MOV,
-        // INC D x  -> 0b1 110 x 00000 <dest> <jmp>
-        INC,
-        DEC,
-        // NEG D    -> 0b1 001 0 00111 <dest> <jmp>
-        // NEG A    -> 0b1 001 0 11001 <dest> <jmp>
-        // NEG M    -> 0b1 001 1 11001 <dest> <jmp>
-        NEG,
-        // ADD D x  -> 0b1 001 x 00000 <dest> <jmp>
-        ADD,
-        // SUB D x  -> 0b1 001 x 01001 <dest> <jmp>
-        // SUB x D  -> 0b1 001 x 00011 <dest> <jmp>
-        SUB,
-        // MUL D x  -> 0b1 110 x 00000 <dest> <jmp>
-        MUL,
-        // DIV D x  -> 0b1 111 x 00000 <dest> <jmp>
-        DIV,
-        // NOT D    -> 0b1 010 0 01100 <dest> <jmp>
-        // NOT A    -> 0b1 010 0 10010 <dest> <jmp>
-        // NOT M    -> 0b1 010 1 10010 <dest> <jmp>
-        NOT,
-        // OR D x  -> 0b1 010 x 00000 <dest> <jmp>
-        OR,
-        // AND D x  -> 0b1 010 x 00000 <dest> <jmp>
-        AND,
-        XOR,
-        ROL,
-        ROR,
-        RAL,
-        RAR,
-        // LOAD <ptr> -> 0b0 <ptr>
-        LOAD,
-        // STO <ptr>   -> 0b0 <ptr>
-        STO,
-    };
+    ROM assemble(const std::string& text) {
+        ROM res;
+        std::istringstream in(text);
 
-    enum Register {
-        D, A, M
-    };
+        std::map<std::string, word> labels;
 
-    struct instruction {
-        Register input;
-        std::bitset<3> dest;
-        std::bitset<3> jmp;
-    };
+        int instruction = 0;
+        while(!in.eof()){
+            std::string command; in >> command;
+
+            if(command == "LABEL") {
+                std::string s; in >> s;
+                labels[s] = instruction;
+            }
+            if(command == "GOTO") {
+
+            }
+            else if(command == "CONST") {
+                word value; in >> value;
+                int reg; in >> reg;
+                if(in.peek() == ','){
+
+                }
+            }
+            else if(command == "MOV") {
+
+            }
+            else if(command == "ADD") {
+
+            }
+            else if(command == "NUL") {
+
+            }
+        }
+
+        return res;
+    }
 };
